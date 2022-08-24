fe8c0a9458167e97114c314c5a47f0a0def0b5ed
Tobias Wiklund <tobias.wiklund@gmail.com>
2022-08-24 22:34:54 +0200

    completed chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import "./Expenses.css";
-import ExpensesList from "./ExpensesList";
 
 const Expenses = ({ items }) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -20,6 +21,7 @@ const Expenses = ({ items }) => {
         onChangeFilter={changeFilterHandler}
         selected={filteredYear}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
